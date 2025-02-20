60ae871e42d6f21bd784379a8d1d81dce1dd8003
Alice <alicelopess.art@gmail,com>
2025-02-20 20:39:31 +0000

    variaveis char criadas

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,8 +6,10 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
-            int numero = 2;
-            Console.WriteLine(numero);
+            //int numero = 2;
+            //Console.WriteLine(numero);
+            char symbol = \u2626;
+            Console.WriteLine(symbol);
         }
     }
 }
