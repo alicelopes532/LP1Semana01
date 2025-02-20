a5b822a234cabcedf654093d608ba31721e8b7f5
Alice <alicelopess.art@gmail,com>
2025-02-20 21:21:32 +0000

    exercicio 6

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,13 +6,16 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            double xx = 1.23456;
+            Console.WriteLine($"x={x:f2}");
+            int ii = $"x=19";
             //string s = "lalalala\grrr\u271D";
             //Consoçe.WriteLine(s)
             //Console.WriteLine("Hello LP1!");
-            string g = "A " + "B " + "C ";
-            string f = $"{y} mais {x} e' igual a {y+x}";
-            Console.WriteLine(g);
-            Console.WriteLine(f);
+            //string g = "A " + "B " + "C ";
+            //string f = $"{y} mais {x} e' igual a {y+x}";
+            //Console.WriteLine(g);
+            //Console.WriteLine(f);
         }
     }
 }
