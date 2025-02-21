1658fc4257d90fc05d5aaaa92c698983b7ca43bf
Alice <alicelopess.art@gmail,com>
2025-02-21 14:50:23 +0000

    ex de avaliacao

---
+++ Percentagem/Program.cs
@@ -0,0 +1,14 @@
+using System Globalization;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere 4 numeros reais entre 0 e 1: ");
+            str = Console.ReadLine(" ");
+            str = $"f={f:f1}";
+        }
+    }
+}
